4b6d7327a6679830400620772790b4fc6dc950fe
Austinbabe <yo861015@gmail.com>
2021-12-20 02:09:41 +0800

    chore: add Clock

---
+++ src/Clock.js
@@ -0,0 +1,33 @@
+const react = require('react')
+
+class Clock extends react.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {date: new Date()}
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(this.updateTime, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
+  updateTime = () => {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>It's {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
+}
+
+export default Clock
