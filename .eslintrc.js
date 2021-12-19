33be7d8cf6f2b870d9d85aae26b6d2ffb1dbe81c
Austinbabe <yo861015@gmail.com>
2021-12-19 22:35:10 +0800

    build: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,11 @@ module.exports = {
     node: true,
     browser: true
   },
-  extends: ['plugin:vue/essential', '@vue/airbnb'],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
+  parser: 'babel-eslint',
   rules: {
     'linebreak-style': ['error', 'unix'],
     semi: [2, 'never'],
-    'comma-dangle': ['error', 'never']
+    'comma-dangle': ['error', 'never'],
+    'jsx-a11y/anchor-is-valid': 'off'
   }
 }
