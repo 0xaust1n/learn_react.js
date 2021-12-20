e89647da2132ec98ceff7236a772cb4beb353dc5
Austin <yo861015@gmail.com>
2021-12-20 18:53:38 +0800

    chore: update App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react'
 import Clock from './Clock'
+import LoginButton from './LoginButton'
 //Class Base Component
 class App extends React.Component {
   render() {
     return (
       <div>
-        <span>Hello {this.props.name}!</span>
-        <App2 name={this.props.name + '!'} />
-        <Clock />
+        {/* <span>Hello {this.props.name}!</span>
+        <App2 name={this.props.name} />
+        <Clock /> */}
+        <LoginButton />
       </div>
     )
   }
 }
 
 const App2 = (props) => {
-  return <div>Hello {props.name}!</div>
+  return (
+    <div>
+      <button onClick={Boom}>{props.name}</button>
+    </div>
+  )
 }
 
-
+const Boom = () => {
+  console.log('BOOM')
+}
 
 export default App
